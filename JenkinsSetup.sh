0973a9067e4fbd320d88cbb4475b5a074fc5f5c5
Ubuntu <ubuntu@ip-172-31-16-151.us-west-2.compute.internal>
2024-05-07 09:15:14 +0000

    Add Jenkins port

--- JenkinsSetup.sh
+++ JenkinsSetup.sh
@@ -13,3 +13,14 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 ###
+# Find the Jenkins WAR file and store the path in a variable
+javawarpath=$(sudo find / -name jenkins.war)
+
+# Check if Jenkins WAR file was found
+if [ -z "$javawarpath" ]; then
+    echo "Jenkins WAR file not found."
+    exit 1
+fi
+
+# Run Jenkins using the Java executable and the path to the WAR file
+sudo java -jar "$javawarpath" --httpPort=8081
