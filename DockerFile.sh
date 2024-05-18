4ae883097314cabf549c802072e5287754fa9d7a
psaineeraj0301 <166980698+psaineeraj0301@users.noreply.github.com>
2024-05-18 10:57:48 +0530

    Update DockerFile.sh

--- DockerFile.sh
+++ DockerFile.sh
@@ -18,6 +18,7 @@ sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
 
+
 # install docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64 -o /tmp/docker-compose
 
@@ -25,3 +26,11 @@ ls -lart /tmp/docker-compose
 sudo chmod +x /tmp/docker-compose
 tmp/docker-compose --version
 sudo mv /tmp/docker-compose /usr/local/bin
+
+
+# Docker permission to run from Jenkins
+ls -l /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker 
+sudo systemctl status docker
+sudo systemctl restart jenkins
