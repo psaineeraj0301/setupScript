2bfaa19145427df3e2596617480cd8ae8a66469e
psaineeraj0301 <166980698+psaineeraj0301@users.noreply.github.com>
2024-05-18 10:58:49 +0530

    Create Grafana.sh

---
+++ Grafana.sh
@@ -0,0 +1,6 @@
+# Installation steps for Grafana
+
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.0.0_amd64.deb
+sudo dpkg -i grafana-enterprise_11.0.0_amd64.deb
+# https://grafana.com/docs/grafana/latest/setup-grafana/start-restart-grafana/
